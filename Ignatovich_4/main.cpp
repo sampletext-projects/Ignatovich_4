3cb341aae1ddfc2d439673f6095408a5f85d9d5b
sampletext32 <sampletext32@bk.ru>
2020-05-01 17:28:14 +0300

    Add project

---
+++ Ignatovich_4/main.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double A[5];
+	double B[5];
+
+	cout << "A: " << endl;
+	for (int i = 0; i < 5; i++)
+	{
+		cout << "[" << i << "] = ";
+		cin >> A[i];
+	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (A[i] >= 0)
+		{
+			B[i] = 1;
+		}
+		else
+		{
+			B[i] = 0;
+		}
+	}
+
+	cout << "B: ";
+	for (int i = 0; i < 5; i++)
+	{
+		cout << B[i] << " ";
+	}
+	cout << endl;
+
+	system("pause");
+	return 0;
+}
